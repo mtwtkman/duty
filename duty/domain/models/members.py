70e7f71d28ba9b4cebc1b7b684b303addc89dea6
mtwtkman <punipuniomochi@gmail.com>
2017-12-24 10:37:35 +0900

    reorder

--- duty/domain/models/members.py
+++ duty/domain/models/members.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django_bulk_update.helper import bulk_update
 
 from . import AsDictMixin
 from .meeting import FacilitatorOrder
@@ -12,6 +13,14 @@ class Member(AsDictMixin, models.Model):
         max_order = FacilitatorOrder.objects.all().aggregate(models.Max('order'))['order__max'] or 0
         FacilitatorOrder.objects.create(member_id=self.pk, order=max_order+1)
         return self
+
+    def delete(self):
+        super().delete()
+        updates = []
+        for i, x in enumerate(FacilitatorOrder.objects.all().order_by('order'), 1):
+            x.order = i
+            updates.append(x)
+        bulk_update(updates, update_fields=['order'])
 
     class Meta:
         db_table = 'members'
